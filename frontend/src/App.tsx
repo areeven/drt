6076d61522458c0ec405c7a9b5badcb27a6069db
areeven <emilie.ost1990@gmail.com>
2024-03-23 11:05:15 +0100

    Add tailwind and image

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import drtLogo from "./shared/images/drt-logo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <img
+          src={drtLogo}
+          alt="den rullande tärningen"
+          width="900"
+          height="auto"
+          className="absolute p-4 text-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+        />
+      </div>
     </div>
   );
 }
