200796b7a048c18841b37996bf7f9ecc6b09cec4
areeven <emilie.ost1990@gmail.com>
2024-03-26 11:00:30 +0100

    Remove comments

--- app/src/server.ts
+++ app/src/server.ts
@@ -3,36 +3,31 @@ import { Application } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import helmet from "helmet";
 import bodyParser from "body-parser";
-// Create the express app and  import the type of app from express;
+
 export const app: Application = express();
 import userRoutes from "./routes/UserRoutes";
 
 app.use(bodyParser.json());
-// Cors
 app.use(cors());
-//configure env;
 dotenv.config();
-// Parser
+app.use(helmet());
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
-// Declare The PORT Like This
 const port: number = 3000;
 
-// Routes
 const router = express.Router();
 userRoutes(router);
 app.use("/api", router);
 
-// Listen the server
 app.listen(port, async () => {
   console.log(`🗄️  Server Fire on http:localhost//${port}`);
 
-  // Connect To The Database
   try {
     await mongoose.connect(process.env.MONGODB_URL as string);
     console.log("🛢️  Connected To Database");
